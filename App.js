9da7ab7df5b949313140f895be56c675d89f1dc4
Ajay Chawada <ajay@Ajays-MacBook-Air.local>
2023-10-14 13:10:32 +0530

    Added splash screen package

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './src/navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <Navigation />
